90cf574c98f6112fa8728dca3b3e433448d89343
Navkaran-singh01 <manveerrandhawa0055@gmail.com>
2025-09-03 23:14:21 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+// Get the backend URL from the environment variables
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api/v1";
+
+// Create an Axios instance with the base URL configured
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
-  withCredentials: true,
+  baseURL: API_URL,
+  withCredentials: true, // To send cookies with requests
 });
